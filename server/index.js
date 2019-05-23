f95aad43f5dc7294eae5d48def8c633fc178152e
name <email@email.com>
2019-05-23 02:37:09 +0100

    Validation: Check user entered Fibonacci index is valid - bug fix

--- server/index.js
+++ server/index.js
@@ -10,73 +10,78 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Postgres Client Setup
-const { Pool } = require('pg');
+const {Pool} = require('pg');
 const pgClient = new Pool({
-  user: keys.pgUser,
-  host: keys.pgHost,
-  database: keys.pgDatabase,
-  password: keys.pgPassword,
-  port: keys.pgPort
+    user: keys.pgUser,
+    host: keys.pgHost,
+    database: keys.pgDatabase,
+    password: keys.pgPassword,
+    port: keys.pgPort
 });
 pgClient.on('error', () => console.log('Lost PG connection'));
 
 pgClient
-  .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-  .catch(err => console.log(err));
+    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch(err => console.log(err));
 
 // Redis Client Setup
 const redis = require('redis');
 const redisClient = redis.createClient({
-  host: keys.redisHost,
-  port: keys.redisPort,
-  retry_strategy: () => 1000
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
 });
 const redisPublisher = redisClient.duplicate();
 
 // Express route handlers
 
 app.get('/', (req, res) => {
-  res.send('Hi');
+    res.send('Hi');
 });
 
 app.get('/values/all', async (req, res) => {
-  const values = await pgClient.query('SELECT * from values');
+    const values = await pgClient.query('SELECT * from values');
 
-  res.send(values.rows);
+    res.send(values.rows);
 });
 
 app.get('/values/current', async (req, res) => {
-  redisClient.hgetall('values', (err, values) => {
-    res.send(values);
-  });
+    redisClient.hgetall('values', (err, values) => {
+        res.send(values);
+    });
 });
 
 app.post('/values', async (req, res) => {
-  const index = req.body.index;
+    const index = req.body.index;
 
-  if (isNaN(index)) {
-    return res.status(422).send('Index not a number');
-  }
+    try {
+        if (isNaN(index)) {
+            return res.status(422).send('Index not a number');
+        }
 
-  if (! Number.isInteger(index)) {
-    return res.status(423).send('Index not an integer');
-  }
+        if (!Number.isInteger(Number(index))) {
+            return res.status(423).send('Index not an integer');
+        }
 
-  if (parseInt(index) < 2) {
-    return res.status(424).send('Index too low');
-  }
+        if (parseInt(index) < 2) {
+            return res.status(424).send('Index too low');
+        }
 
-  if (parseInt(index) > 40) {
-    return res.status(425).send('Index too high');
+        if (parseInt(index) > 40) {
+            return res.status(425).send('Index too high');
+        }
+    } catch (err) {
+        console.log('Bad index' + err)
+    }
 
-  }
-  redisClient.hset('values', index, 'Nothing yet!');
-  redisPublisher.publish('insert', index);
-  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+redisClient.hset('values', index, 'Nothing yet!');
+redisPublisher.publish('insert', index);
+pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
 
-  res.send({ working: true });
-});
+res.send({working: true});
+})
+;
 
 app.listen(5000, err => {
-  console.log('Listening');
+    console.log('Listening');
 });
